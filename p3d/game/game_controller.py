1651ce2feb759632f0ad99d1f2cb8a790ea0bee0
gadik <gdkln3030@gmail.com>
2026-03-12 22:43:24 +0200

    fix: update import from createDisk to createAnnulus

--- p3d/game/game_controller.py
+++ p3d/game/game_controller.py
@@ -12,7 +12,7 @@ from panda3d.core import (
 )
 
 from game.geo_challenge_game import DifficultyLevel, GeoChallengeGame
-from game.game_markers import createDisk, createTargetRings, createXMark, createCityLabel
+from game.game_markers import createAnnulus, createTargetRings, createXMark, createCityLabel
 from gui.game_gui_controller import GameGuiController
 from world_data_manager import WorldDataManager
 
