38a1a1533f9cf910097ebc9d7a06e3ba40ee489a
Your Name <GADKL@rafael.co.il>
2026-03-18 11:21:35 +0200

    Update commit ID to 8369e5b

--- web/js/version.js
+++ web/js/version.js
@@ -3,7 +3,7 @@
  * Update this file on each release/merge
  */
 const APP_VERSION = 'v1.00';
-const APP_COMMIT = 'c5474b3';
+const APP_COMMIT = '8369e5b';
 
 // Update version display on page load
 document.addEventListener('DOMContentLoaded', () => {
